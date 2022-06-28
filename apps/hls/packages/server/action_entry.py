42802d98dc6f44285eba0878f32b39eb65ab9a07
peng guanghong <pengguanghong@wonderent.net>
2022-06-28 11:35:27 +0800

    test

---
+++ apps/hls/packages/server/action_entry.py
@@ -0,0 +1,12 @@
+from job_manager.packages.easy_tools import job_start_before, task_runner_celery
+
+
+def update_entry(username, cmd_list):
+    """
+    更新
+    :param username: 执行者
+    :param cmd_list: 任务命令列表
+    :return:
+    """
+    job_task, job_cmds = job_start_before("更新", "pengguanghong", update_entry, cmd_list)
+    return task_runner_celery(job_task, job_cmds)
