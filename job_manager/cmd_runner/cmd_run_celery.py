eb848032e832e9d1938bb9209d8044c5d36df3a3
peng guanghong <pengguanghong@wonderent.net>
2022-07-01 15:00:22 +0800

    1

--- job_manager/cmd_runner/cmd_run_celery.py
+++ job_manager/cmd_runner/cmd_run_celery.py
@@ -15,7 +15,7 @@ def runner(cmd, timeout=300):
     timer.daemon = True
     timer.start()
     out, err = p.communicate()
-    out.decode('utf-8')
+    out = out.decode('utf-8')
     err = err.decode('utf-8')
     if "Python 3.6 is no longer supported by the Python core team." in err:
         err = ""
